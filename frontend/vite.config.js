8875dde96d499a2d7b4aa899372377294b02f1e1
YuvrajBhardawaj <126788904+YuvrajBhardawaj@users.noreply.github.com>
2024-07-29 19:01:06 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-server: {
-  proxy: {
-    '/api': {
-      target: 'https://todolist-backend-w1l0.onrender.com',
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, ''),
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://todolist-backend-w1l0.onrender.com',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
     },
   },
-},
   plugins: [react()],
 })
