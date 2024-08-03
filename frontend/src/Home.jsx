6c15e719d0daa1c9694b62f6f867003e917a9f76
YuvrajBhardawaj <yuvrajbhardawaj31018@gmail.com>
2024-08-03 15:00:57 +0530

    New BugFree Codes

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 function Home() {
   return (
     <div className="flex flex-col justify-center items-center h-screen bg-black">
-      <h1 id="heading" className="text-center">Create your To Do List</h1>
+      <h1 id="heading" className="text-center">Create your To Do List🌄🌄🌄</h1>
       <Link to="/SignIn"><button className='mt-4 btn btn-dark bg-dark py-2'>SignIn/SignUp</button></Link>
     </div>
   )
